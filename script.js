55154966d34a236eda2e85ea709cf986e1f30345
Chris <hainesy91@gmail.com>
2018-08-08 10:01:33 +0100

    moved the script code into the dedicated script file and then linked to it from html file

---
+++ script.js
@@ -0,0 +1,10 @@
+//Declaring a variable to get the class on the button in the html
+var button = document.getElementsByClassName("button");
+
+//Accesing an element in the array, and then assigning an onclick method and a function to the variable
+button[0].onclick = handleClick;
+
+//Creating a function that sends an alert everytime the function is called because we love alerts like Neelam!
+function handleClick () {
+  alert("We should drink rose");
+};
