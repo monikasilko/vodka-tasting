a2d96fa96d3aeafb25c1e7acd23660dfdf921ec4
Chris <hainesy91@gmail.com>
2018-08-08 22:46:00 +0100

    added new files and updated the functions, elements and styling in each one

---
+++ script.js
@@ -0,0 +1,21 @@
+//Declaring a variable to get the class on the button in the html
+var button = document.getElementsByClassName("button");
+
+//Accesing an element in the array, and then assigning an onclick method and a function to the variable
+button[0].onclick = handleClick;
+
+//Creating a function that sends an alert everytime the function is called because we love alerts like Neelam!
+function handleClick () {
+  alert("We should drink rose");
+};
+
+//Function that gets the divs id and changes the style of it to display
+function showDiv () {
+  document.getElementById('surprise').style.display = "inline";
+  document.getElementById('clear').style.display = "inline";
+}
+
+function removeDiv () {
+  document.getElementById('surprise').style.display = 'none';
+  document.getElementById('clear').style.display = 'none';
+}
